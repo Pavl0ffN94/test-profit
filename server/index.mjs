65d90ade918a673fd197c56839336c896cf602e0
PAvl0ffN <pavl0ffn94@gmail.com>
2024-08-29 17:44:59 +0400

    fix versel build

--- server/index.mjs
+++ server/index.mjs
@@ -4,7 +4,8 @@ import cors from 'cors';
 import { readDataFromFile, writeDataToFile, generateUniqueId } from './helpers.mjs';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(bodyParser.json());
